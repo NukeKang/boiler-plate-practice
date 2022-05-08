1b1969218ed002bfbb8f86849e04190cf72c16cc
rkdalstjd224 <rkdalstjd224@gmail.com>
2022-05-09 00:06:20 +0900

    Feat. 비밀 정보 보호 추가

--- index.js
+++ index.js
@@ -4,15 +4,33 @@ const app = express();
 const port = 2000;
 
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+//application/x-www-from-unlencoded
+app.use(express.urlencoded({ extended: true }));
+//application/json
+app.use(express.json());
+
 mongoose
-  .connect(
-    "mongodb+srv://kang:abcd1234@cluster0.wg0on.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("mongoDB Connected!"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello FXXKING World!");
+  res.send("Hello World! 어려워요");
+});
+app.post("/register", (req, res) => {
+  // 회원가입에 필요한 정보들을 clinet에서 가져오면
+  // 그것들을 db에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ error: false, err });
+
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
